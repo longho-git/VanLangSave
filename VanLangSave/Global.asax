f4590046c0283d8a1113d3d610997a7563300b6a
To Long Ho <kuho123456789@gmail.com>
2021-07-31 23:14:54 +0700

    update project

---
+++ VanLangSave/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VanLangSave.MvcApplication" Language="C#" %>
---
+++ VanLangSave/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VanLangSave.Startup))]
+namespace VanLangSave
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
